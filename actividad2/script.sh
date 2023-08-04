16f3d338b099741a8e2668bc18881bcc2eeb38e2
Daniel Dubón <3179425811504@ingenieria.usac.edu.gt>
2023-08-04 01:26:06 -0600

    update

---
+++ actividad2/script.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Pide el nombre de usuario de GitHub y lo almacena en la variable GITHUB_USER
+echo "Introduce tu nombre de usuario de GitHub:"
+read GITHUB_USER
+
+# Realilza la petición GET a la API de GitHub para obtener los datos del usuario
+RES=$(curl -s https://api.github.com/users/$GITHUB_USER)
+
+# Almacena en variables los datos del usuario
+ID=$(echo $RES | jq '.id')
+CREATED_AT=$(echo $RES | jq '.created_at')
+
+# Imprime un mensaje con los datos del usuario
+MESSAGE="Hola $GITHUB_USER. User ID: $ID. Cuenta fue creada el: $CREATED_AT."
+echo $MESSAGE
+
+# Almacena en la variable FECHA la fecha actual
+FECHA=$(date +%Y-%m-%d)
+
+# Crea el directorio
+mkdir -p "/tmp/$FECHA"
+
+echo $MESSAGE >> "/tmp/$FECHA/saludos.log"
